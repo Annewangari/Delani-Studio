e4fb5b3eb9df66811f8d4cb00c22d320c4abcf69
Annewangari <annekare3@gmail.com>
2020-08-29 15:55:50 +0300

    add jquery for development in script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,19 @@
-$(document).ready(function(){
-    $("#design-pic").click(function(){
-    $(".design-a").hide(1500).toggle();
-       $("design-pic").slideUp();
- 
+$(document).ready(function () {
+    $("#design-pic").click(function () {
+        $(".design-a").hide(1500).toggle();
+        $("design-pic").slideUp();
+
     });
-    $(".design-a").click(function(){
-      $("#design-pic").slideDown();
-      $(".design-a").hide();
+    $(".design-a").click(function () {
+        $("#design-pic").slideDown();
+        $(".design-a").hide();
+    })
+    $("#development-image").click(function () {
+        $(".developlement").hide(1500).toggle();
+        $("#development-image").slideUp();
+    });
+    $(".developlement").click(function () {
+        $("#development-image").slideDown();
+        $(".developlement").hide();
     })
 })

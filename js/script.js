f44fda1484e8a5b0750715df6c1950ecaa187532
Annewangari <annekare3@gmail.com>
2020-08-29 17:15:28 +0300

    change code in jquery for design and products

--- js/script.js
+++ js/script.js
@@ -1,10 +1,30 @@
-$(document).ready(function(){
-    $("#development-image").click(function(){
-      $("#development-image").slideDown('1500').hide('1000');
-      $(".development").show('1500');
+$(document).ready(function () {
+    $("#development-image").click(function () {
+        $("#development-image").slideDown('1500').hide('1000');
+        $(".development").show('1500');
     });
-    $(".development").click(function(){
-      $(".development").slideUp('1500');
-      $("#development-image").slideDown('1500');
+    $(".development").click(function () {
+        $(".development").slideUp('1500');
+        $("#development-image").slideDown('1500');
+    });
+});
+$(document).ready(function () {
+    $("#design-pic").click(function () {
+        $("#design-pic").slideDown('1500').hide('1000');
+        $(".design-a").show('1500');
+    });
+    $(".design-a").click(function () {
+        $(".design-a").slideUp('1500');
+        $("#design-pic").slideDown('1500');
+    });
+});
+$(document).ready(function(){
+    $("#product-image").click(function(){
+      $("#product-image").slideDown('1500').hide('1000');
+      $(".product").show('1500');
+    });
+    $(".product").click(function(){
+      $(".product").slideUp('1500');
+      $("#product-image").slideDown('1500');
     });
   });
